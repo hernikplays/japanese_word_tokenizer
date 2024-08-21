6845f02dbae92265829fb1587f7bb12e5c6a267c
Matyáš Caras <matyas@caras.wtf>
2024-08-21 11:40:40 +0200

    fix: don't depend on flutter
    also wrote the test as an actual test

--- test/japanese_word_tokenizer_test.dart
+++ test/japanese_word_tokenizer_test.dart
@@ -1,7 +1,10 @@
 import 'package:japanese_word_tokenizer/japanese_word_tokenizer.dart';
+import 'package:test/test.dart';
 
 void main() {
-  String text = 'ここでテキストを分かち書きします';
-  List<dynamic> tokens = tokenize(text);
-  print(tokens);
+  test('Tokenization', () {
+    String text = 'ここでテキストを分かち書きします';
+    final tokens = tokenize(text);
+    expect(tokens, ["ここで", "テキスト", "を", "分か", "ち", "書き", "します"]);
+  });
 }

cf808e9fbcf34272a025ca8e8a6afb18d07f50e6
Matyáš Caras <matyas@caras.wtf>
2024-08-21 11:39:25 +0200

    docs: remove example

--- example/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:japanese_word_tokenizer/japanese_word_tokenizer.dart';
-
-void main() {
-  String text = 'ここでテキストを分かち書きします';
-  List<dynamic> tokens = tokenize(text);
-  print(tokens);
-}
